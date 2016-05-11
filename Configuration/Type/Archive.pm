6f745cb60cda0b4f5782ffbc41b2dee6c7e18fc5
Jan Gehring <jan.gehring@dm.de>
2016-05-11 14:34:53 +0200

    recursive archive

--- Configuration/Type/Archive.pm
+++ Configuration/Type/Archive.pm
@@ -1,7 +1,7 @@
 #
 # vim: set ts=2 sw=2 tw=0:
 # vim: set expandtab:
-   
+
 package Application::Configuration::Type::Archive;
 
 use Moose;
@@ -18,22 +18,27 @@ use Application::Configuration::Fs::File;
 use Data::Dumper;
 
 has files => (
-  is => 'ro',
-  isa => 'ArrayRef[Application::Configuration::Fs::File]',
-  lazy => 1,
+  is      => 'ro',
+  isa     => 'ArrayRef[Application::Configuration::Fs::File]',
+  lazy    => 1,
   default => sub {
     my ($self) = @_;
     my @files;
 
-    my $extracted_dir = Application::Download::get($self->source, extract => 1);
+    my $extracted_dir =
+      Application::Download::get( $self->source, extract => 1 );
 
     my @content;
-    Rex::Commands::LOCAL(sub {
-      @content = grep { $_ !~ m/^\./ && is_dir("$extracted_dir/$_") } list_files $extracted_dir;
-    });
+    Rex::Commands::LOCAL(
+      sub {
+        @content =
+          grep { $_ !~ m/^\./ && is_dir("$extracted_dir/$_") }
+          list_files $extracted_dir;
+      }
+    );
 
     my $config_dir;
-    if(scalar @content == 1) {
+    if ( scalar @content == 1 ) {
       $config_dir = "$extracted_dir/$content[0]";
     }
     else {
@@ -41,15 +46,25 @@ has files => (
     }
 
     Rex::Commands::LOCAL sub {
-      push @files, map {
-            Application::Configuration::Fs::File->new(
-              lookup_path   => File::Spec->catfile($config_dir, $_),
-              configuration => $self,
-            )
+      my @dirs = ($config_dir);
+
+      for my $d (@dirs) {
+        for my $entry ( list_files($d) ) {
+          if ( is_dir( File::Spec->catfile( $d, $entry ) ) ) {
+            push @dirs, File::Spec->catfile( $d, $entry );
+            next;
           }
-          grep {
-            -f File::Spec->catfile($config_dir, $_)
-          } list_files($config_dir);
+
+          my $name = File::Spec->catfile( $d, $entry );
+          $name =~ s/^\Q$config_dir\E\/?//;
+
+          push @files, Application::Configuration::Fs::File->new(
+            lookup_path   => File::Spec->catfile( $d, $entry ),
+            configuration => $self,
+            name          => $name,
+          );
+        }
+      }
     };
 
     return \@files;
