39b048fcc6dda9f2054de7bea2a33b0f579ae96d
Jan Gehring <jan.gehring@dm.de>
2016-06-10 10:19:47 +0200

    use internal ua for better error handling

--- Download/Http.pm
+++ Download/Http.pm
@@ -5,6 +5,7 @@ use File::Basename;
 use Carp;
 require Rex::Commands;
 require Rex::Commands::Download;
+use LWP::UserAgent;
 
 extends qw(Application::Download::Base);
 
@@ -26,12 +27,34 @@ sub download {
     rmdir $tmp_dir;
     mkdir $tmp_dir;
 
-    Rex::Commands::Download::download( $url->to_s_without_auth,
-      "$tmp_dir/" . File::Basename::basename($url), %dl_option );
-
     my $deploy_file = File::Basename::basename($url);
-
     $dl_file = "$tmp_dir/$deploy_file";
+
+    my $ua = LWP::UserAgent->new;
+    if($ENV{http_proxy} || $ENV{https_proxy}) {
+      Rex::Logger::info("Loading http proxy settings from environment:");
+      Rex::Logger::info("http_proxy = " . ($ENV{http_proxy} || ''));
+      Rex::Logger::info("https_proxy = " . ($ENV{https_proxy} || ''));
+      $ua->env_proxy;
+    }
+
+    open(my $fh, ">", $dl_file) or die("Error opening file to write ($dl_file): $!");
+    my $response = $ua->get(
+      $url->to_s,
+      ':content_cb' => sub {
+        my ( $data, $response, $protocol ) = @_;
+        print $fh $data;
+      }
+    );
+    close $fh;
+
+    if ( !$response->is_success ) {
+      unlink $dl_file;
+      die "Error downloading file from server.\nStatus: " . $response->status_line;
+    }
+    
+    return $dl_file;
+    
   };
 
   if ( !-f $dl_file ) {
